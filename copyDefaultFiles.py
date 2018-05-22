71b5adb7ca05da62e71d83dbf280eff7d646fff1
John Allwine <jallwine86@gmail.com>
2018-05-22 15:56:15 +0000

    added compensation tables for a and b axes along with interface for editing them

--- copyDefaultFiles.py
+++ copyDefaultFiles.py
@@ -7,8 +7,21 @@ import shutil
 ROOT_DIRECTORY = "/home/pocketnc/pocketnc"
 TOOL_TABLE_FILE = os.path.join(ROOT_DIRECTORY, "Settings/tool.tbl")
 TOOL_TABLE_DEFAULT_FILE = os.path.join(ROOT_DIRECTORY, "Settings/tool.tbl.default")
+
 CLIENT_CONFIG_FILE = os.path.join(ROOT_DIRECTORY, "Rockhopper/CLIENT_CONFIG.JSON")
 CLIENT_CONFIG_DEFAULT_FILE = os.path.join(ROOT_DIRECTORY, "Rockhopper/CLIENT_CONFIG.JSON.default")
+
+A_COMP_FILE = os.path.join(ROOT_DIRECTORY, "Settings/a.comp");
+A_COMP_DEFAULT_FILE = os.path.join(ROOT_DIRECTORY, "Settings/a.comp.default");
+
+B_COMP_FILE = os.path.join(ROOT_DIRECTORY, "Settings/b.comp");
+B_COMP_DEFAULT_FILE = os.path.join(ROOT_DIRECTORY, "Settings/b.comp.default");
+
+if not os.path.isfile(A_COMP_FILE):
+    shutil.copyfile(A_COMP_DEFAULT_FILE, A_COMP_FILE)
+
+if not os.path.isfile(B_COMP_FILE):
+    shutil.copyfile(B_COMP_DEFAULT_FILE, B_COMP_FILE)
 
 if not os.path.isfile(TOOL_TABLE_FILE):
     shutil.copyfile(TOOL_TABLE_DEFAULT_FILE, TOOL_TABLE_FILE)
