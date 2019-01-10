15467c8a5b669c197137027d9e7c029cba4d4ef8
John Allwine <john@pocketnc.com>
2019-01-10 23:25:34 +0000

    source /home/pocketnc/.pocketnc_env file to be able to adjust environment variables on different systems
    SOFT-130
    SOFT-62

--- startPocketNC.sh
+++ startPocketNC.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -f /home/pocketnc/.pocketnc_env ]; then
+  source /home/pocketnc/.pocketnc_env
+fi
+
+export PATH=$PATH:/home/pocketnc/pocketnc/Settings
+
 /home/pocketnc/pocketnc/Settings/generateINI.py
 
 linuxcnc /home/pocketnc/pocketnc/Settings/PocketNC.ini
--- startRockhopper.sh
+++ startRockhopper.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+if [ -f /home/pocketnc/.pocketnc_env ]; then
+  source /home/pocketnc/.pocketnc_env
+fi
+
 python /home/pocketnc/pocketnc/Rockhopper/LinuxCNCWebSktSvr.py /home/pocketnc/pocketnc/Settings/PocketNC.ini
