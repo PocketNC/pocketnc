7f2b5786ca009cd63d28508712dc4689197e0a14
Jake Danczyk <jake@pocketnc.com>
2020-08-17 19:28:35 +0000

    v4.7.2 Explicitly check for and kill hss_sensor HAL components in stopPocketNC.sh

--- stopPocketNC.sh
+++ stopPocketNC.sh
@@ -20,3 +20,8 @@ pgrep linuxcnc | while read -r pid ; do
   echo "Timeout expired, force killing LinuxCNC process $pid"
   kill -9 $pid
 done
+
+grep hss_sensors | while read -r pid ; do
+  echo "Timeout expired, force killing hss_sensors HAL component $pid"
+  kill -9 $pid
+done
