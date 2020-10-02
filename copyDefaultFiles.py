4302298f59fc4d9ae15708e765b7fb1c68a8c4e4
John Allwine <john@pocketnc.com>
2020-10-02 23:23:40 +0000

    initial machinekit -> emcapplication changes

--- copyDefaultFiles.py
+++ copyDefaultFiles.py
@@ -17,11 +17,17 @@ A_COMP_DEFAULT_FILE = os.path.join(ROOT_DIRECTORY, "Settings/a.comp.default");
 B_COMP_FILE = os.path.join(ROOT_DIRECTORY, "Settings/b.comp");
 B_COMP_DEFAULT_FILE = os.path.join(ROOT_DIRECTORY, "Settings/b.comp.default");
 
+C_COMP_FILE = os.path.join(ROOT_DIRECTORY, "Settings/c.comp");
+C_COMP_DEFAULT_FILE = os.path.join(ROOT_DIRECTORY, "Settings/c.comp.default");
+
 if not os.path.isfile(A_COMP_FILE):
     shutil.copyfile(A_COMP_DEFAULT_FILE, A_COMP_FILE)
 
 if not os.path.isfile(B_COMP_FILE):
     shutil.copyfile(B_COMP_DEFAULT_FILE, B_COMP_FILE)
+
+if not os.path.isfile(C_COMP_FILE):
+    shutil.copyfile(C_COMP_DEFAULT_FILE, C_COMP_FILE)
 
 if not os.path.isfile(TOOL_TABLE_FILE):
     shutil.copyfile(TOOL_TABLE_DEFAULT_FILE, TOOL_TABLE_FILE)
