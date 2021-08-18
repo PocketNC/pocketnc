668b05072d31f07263af32592ad76e252db18255
Jake Danczyk <jake@pocketnc.com>
2021-08-18 14:10:39 -0600

    SOFT-44 run .pocketnc_env within enableServices.sh

--- enableServices.sh
+++ enableServices.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f /home/pocketnc/.pocketnc_env ]; then
+  source /home/pocketnc/.pocketnc_env
+fi
+
 systemctl enable ${POCKETNC_DIRECTORY}Rockhopper.service
 systemctl enable ${POCKETNC_DIRECTORY}PocketNC.service
 #systemctl enable /home/pocketnc/pocketnc/pocketnc-ui.service
