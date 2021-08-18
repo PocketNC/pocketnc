309088975df768486e588f0c6a6230ec6a1bed70
Jake Danczyk <jake@pocketnc.com>
2021-08-18 10:18:02 -0600

    SOFT-44 Use POCKETNC_DIRECTORY in enableServices.sh

--- enableServices.sh
+++ enableServices.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-systemctl enable /home/pocketnc/pocketnc/Rockhopper.service
-systemctl enable /home/pocketnc/pocketnc/PocketNC.service
+systemctl enable ${POCKETNC_DIRECTORY}Rockhopper.service
+systemctl enable ${POCKETNC_DIRECTORY}PocketNC.service
 #systemctl enable /home/pocketnc/pocketnc/pocketnc-ui.service
