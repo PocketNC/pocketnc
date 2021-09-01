b3cac7f4683c646d72c2968d80946e392b5228ff
John Allwine <john@pocketnc.com>
2021-09-01 19:50:38 +0000

    create usb directories at start up to ensure thumbdrive can be mounted properly

--- startPocketNC.sh
+++ startPocketNC.sh
@@ -9,6 +9,31 @@ fi
 
 sudo ${POCKETNC_DIRECTORY}/ensureBootloaderUpToDate.sh
 
+if [ ! -d ${POCKETNC_USB_DIRECTORY}/usb0 ]; then
+  sudo mkdir -p ${POCKETNC_USB_DIRECTORY}/usb0
+fi
+if [ ! -d ${POCKETNC_USB_DIRECTORY}/usb1 ]; then
+  sudo mkdir -p ${POCKETNC_USB_DIRECTORY}/usb1
+fi
+if [ ! -d ${POCKETNC_USB_DIRECTORY}/usb2 ]; then
+  sudo mkdir -p ${POCKETNC_USB_DIRECTORY}/usb2
+fi
+if [ ! -d ${POCKETNC_USB_DIRECTORY}/usb3 ]; then
+  sudo mkdir -p ${POCKETNC_USB_DIRECTORY}/usb3
+fi
+if [ ! -d ${POCKETNC_USB_DIRECTORY}/usb4 ]; then
+  sudo mkdir -p ${POCKETNC_USB_DIRECTORY}/usb4
+fi
+if [ ! -d ${POCKETNC_USB_DIRECTORY}/usb5 ]; then
+  sudo mkdir -p ${POCKETNC_USB_DIRECTORY}/usb5
+fi
+if [ ! -d ${POCKETNC_USB_DIRECTORY}/usb6 ]; then
+  sudo mkdir -p ${POCKETNC_USB_DIRECTORY}/usb6
+fi
+if [ ! -d ${POCKETNC_USB_DIRECTORY}/usb7 ]; then
+  sudo mkdir -p ${POCKETNC_USB_DIRECTORY}/usb7
+fi
+
 if [ -f ${POCKETNC_VAR_DIRECTORY}/PocketNC.ini ];then
   rm ${POCKETNC_VAR_DIRECTORY}/PocketNC.ini
 fi
