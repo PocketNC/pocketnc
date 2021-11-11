503f81b6538003fede0213d96301b57ea31b52f9
John Allwine <john@pocketnc.com>
2021-11-11 21:49:42 +0000

    check second partition of eMMC for calibration data as well SOFT-781

--- copyFilesFromEMMC.sh
+++ copyFilesFromEMMC.sh
@@ -3,6 +3,7 @@
 if [ -b /dev/mmcblk1p1 ]; then
   sudo mount /dev/mmcblk1p1 /mnt
 
+  # Handles calibration data for machines made afer August 2019
   if [ -f /mnt/home/pocketnc/pocketnc/Settings/CalibrationOverlay.inc ]; then
     cp /mnt/home/pocketnc/pocketnc/Settings/CalibrationOverlay.inc ${POCKETNC_VAR_DIRECTORY}/CalibrationOverlay.inc
     python3 ${POCKETNC_DIRECTORY}/Rockhopper/convertCalibrationOverlay.py ${POCKETNC_VAR_DIRECTORY}/CalibrationOverlay.inc
@@ -34,6 +35,7 @@ fi
 if [ -b /dev/mmcblk1p2 ]; then
   sudo mount /dev/mmcblk1p2 /mnt
 
+  # Handles v1 calibration data
   if [ -f /mnt/home/pocketnc/linuxcnc/configs/ARM.BeagleBone.PocketNC/PocketNC.ini ]; then
     python3 ${POCKETNC_DIRECTORY}/Rockhopper/convertV1INIFileToOverlay.py \
             /mnt/home/pocketnc/linuxcnc/configs/ARM.BeagleBone.PocketNC/PocketNC.ini \
@@ -44,6 +46,32 @@ if [ -b /dev/mmcblk1p2 ]; then
 
   if [ -d /mnt/home/pocketnc/linuxcnc/nc_files ]; then
     cp -r /mnt/home/pocketnc/linuxcnc/nc_files/*.ngc /home/pocketnc/ncfiles/
+  fi
+
+  # Handles calibration data for v2 machines made prior to September 2019
+  if [ -f /mnt/home/pocketnc/pocketnc/Settings/CalibrationOverlay.inc ]; then
+    cp /mnt/home/pocketnc/pocketnc/Settings/CalibrationOverlay.inc ${POCKETNC_VAR_DIRECTORY}/CalibrationOverlay.inc
+    python3 ${POCKETNC_DIRECTORY}/Rockhopper/convertCalibrationOverlay.py ${POCKETNC_VAR_DIRECTORY}/CalibrationOverlay.inc
+  fi
+
+  if [ -f /mnt/home/pocketnc/pocketnc/Settings/a.comp ]; then
+    cp /mnt/home/pocketnc/pocketnc/Settings/a.comp ${POCKETNC_VAR_DIRECTORY}/a.comp
+  fi
+
+  if [ -f /mnt/home/pocketnc/pocketnc/Settings/b.comp ]; then
+    cp /mnt/home/pocketnc/pocketnc/Settings/b.comp ${POCKETNC_VAR_DIRECTORY}/b.comp
+  fi
+
+  if [ -f /mnt/home/pocketnc/pocketnc/Settings/c.comp ]; then
+    cp /mnt/home/pocketnc/pocketnc/Settings/c.comp ${POCKETNC_VAR_DIRECTORY}/c.comp
+  fi
+
+  if [ -f /mnt/home/pocketnc/pocketnc/Settings/tool.tbl ]; then
+    cp /mnt/home/pocketnc/pocketnc/Settings/tool.tbl ${POCKETNC_VAR_DIRECTORY}/tool.tbl
+  fi
+
+  if [ -d /mnt/home/pocketnc/ncfiles ]; then
+    cp -r /mnt/home/pocketnc/ncfiles/* /home/pocketnc/ncfiles/
   fi
   sudo umount /mnt
 fi
